0f074ba088cba8ede63024d479b23d1fde77526b
James Sullivan <jsullivan8794@gmail.com>
2023-04-24 22:29:30 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.querySelector('main#main')
+undefined
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "James Sullivan is the champion"
